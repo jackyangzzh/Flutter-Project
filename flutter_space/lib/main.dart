dc9af75111e59f912b5d642893d8f32740a7f086
Jack Yang <jackyangzzh@gmail.com>
2019-10-20 10:04:26 -0500

    Add options

--- flutter_space/lib/main.dart
+++ flutter_space/lib/main.dart
@@ -3,15 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favorite food?',
+      'What sports do you play?',
+      'What\'s your favoriate designer',
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Flutter App'),
-        ),
-        body: Text('Some text as placeholder!'),
-      ),
+          appBar: AppBar(
+            title: Text('My Flutter App'),
+          ),
+          body: Column(
+            children: [
+              Text('Question here'),
+              RaisedButton(child: Text('Answer 1'),onPressed: null),
+              RaisedButton(child: Text('Answer 2'),onPressed: null,),
+              RaisedButton(child: Text('Anwer 3'),),
+              RaisedButton(child: Text('Answer 4'),),
+            ],
+          )
+          ),
     );
   }
 }
