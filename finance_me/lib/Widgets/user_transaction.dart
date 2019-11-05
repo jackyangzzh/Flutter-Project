882674b41114ba5d763287504a7b6e23b5bced2c
Jack Yang <jackyangzzh@gmail.com>
2019-11-04 22:36:16 -0600

    Revert "update"
    This reverts commit 1ac6a540bf4f73e28fd5ca526690b36e9a1b5715.

---
+++ finance_me/lib/Widgets/user_transaction.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'new_transaction.dart';
+import 'transaction_list.dart';
+import '../Models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: 'Shoes', amount: 120, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Hoodie', amount: 79.99, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransactions),
+      ],
+    );
+  }
+}
--- finance_me/lib/main.dart
+++ finance_me/lib/main.dart
@@ -1,41 +1,11 @@
-import './Widgets/new_transaction.dart';
+import 'package:finance_me/Widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 import './Widgets/user_transaction.dart';
-import './Models/transaction.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  void startAddNewTransaction(BuildContext ctx) {
-    showModalBottomSheet(context: ctx, builder: (_){
-      return NewTransaction();
-    },);
-  }
-
-  final List<Transaction> _userTransactions = [
-    Transaction(id: 't1', title: 'Shoes', amount: 120, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Hoodie', amount: 79.99, date: DateTime.now()),
-  ];
-
-  void _addNewTransaction(String title, double amount) {
-    final newTx = Transaction(
-        title: title,
-        amount: amount,
-        date: DateTime.now(),
-        id: DateTime.now().toString());
-
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
