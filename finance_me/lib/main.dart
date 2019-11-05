1ac6a540bf4f73e28fd5ca526690b36e9a1b5715
Jack Yang <jackyangzzh@gmail.com>
2019-11-04 22:34:12 -0600

    update

--- finance_me/lib/main.dart
+++ finance_me/lib/main.dart
@@ -1,11 +1,41 @@
-import 'package:finance_me/Widgets/user_transaction.dart';
+import './Widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
 import './Widgets/user_transaction.dart';
+import './Models/transaction.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(context: ctx, builder: (_){
+      return NewTransaction();
+    },);
+  }
+
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: 'Shoes', amount: 120, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Hoodie', amount: 79.99, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
