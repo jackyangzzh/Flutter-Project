da0bb82fbb4e8cd20b10c97cfe4e13e7c76b5249
Jack Yang <jackyangzzh@gmail.com>
2019-11-04 22:53:11 -0600

    WidgetUpdate

--- finance_me/lib/main.dart
+++ finance_me/lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:finance_me/Widgets/user_transaction.dart';
+import './Widgets/transaction_list.dart';
+
+import './Widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
-import './Widgets/user_transaction.dart';
+import './Models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +18,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(onTap: () {}, child: NewTransaction(_addNewTransaction),behavior: HitTestBehavior.opaque,);
+      },
+    );
+  }
+
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: 'Shoes', amount: 120, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Hoodie', amount: 79.99, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +58,7 @@ class MyHomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add_circle_outline),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           )
         ],
       ),
@@ -41,13 +74,15 @@ class MyHomePage extends StatelessWidget {
                 elevation: 3,
               ),
             ),
-            UserTransaction(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:
-          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
     );
   }
 }
