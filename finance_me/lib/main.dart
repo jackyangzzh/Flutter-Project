32d869b856a8336bdeb224d71d4a3e74bfb205c1
Jack <jackyangzzh@gmail.com>
2019-11-07 20:43:16 -0600

    theme

--- finance_me/lib/main.dart
+++ finance_me/lib/main.dart
@@ -12,6 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        accentColor: Colors.purple,
+      ),
       title: 'Finance Me!',
       home: MyHomePage(),
     );
