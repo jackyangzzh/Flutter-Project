2e695893e4cfe8f30558741ca10f19c336873e4f
Jack <jackyangzzh@gmail.com>
2019-11-19 18:39:33 -0600

    category class

---
+++ my_collection/lib/dummy_data.dart
@@ -0,0 +1,8 @@
+import './Model/category.dart';
+import 'package:flutter/material.dart';
+
+const dummyData = const [
+  Category(id: 'c1', title: 'Resturuants', color: Colors.purple),
+  Category(id: 'c2', title: 'Hiking', color: Colors.orange),
+  Category(id: 'c3', title: 'Keychain', color: Colors.red),
+];
