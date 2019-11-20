1ffa5f17317e490c6e5478f9c38b1f03726b0dec
Jack <jackyangzzh@gmail.com>
2019-11-19 19:37:42 -0600

    cateogoryScreen

---
+++ my_collection/lib/category_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String _title;
+  final Color _color;
+
+  CategoryItem(this._title, this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(_title),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            _color.withOpacity(0.6),
+            _color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ));
+  }
+}
--- my_collection/lib/category_screen.dart
+++ my_collection/lib/category_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import './category_item.dart';
+import './dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
+    return Scaffold(
+      appBar: AppBar(title: const Text('My collection')),
+        body: GridView(
+      children: dummyData
+          .map((data) => CategoryItem(data.title, data.color))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20),
-    );
+    ));
   }
 }
