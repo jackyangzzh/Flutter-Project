6d7ff7da30b3ac120b73175c3037049f33bb5bdf
Jack <jackyangzzh@gmail.com>
2020-01-12 09:13:14 +0800

    auth screen start

---
+++ collection_app/lib/Screens/auth_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+enum AuthMode { Signup, Login }
+
+class AuthScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(255, 255, 102, 1).withOpacity(0.5),
+                Color.fromRGBO(102, 255, 102, 1).withOpacity(0.9)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0, 1],
+            )),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              height: deviceSize.height,
+              width: deviceSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
