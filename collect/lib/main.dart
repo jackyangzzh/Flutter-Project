de1c2122a1c9f877ca81f8b4dac3e24aba42ee52
Jack <jackyangzzh@gmail.com>
2020-01-24 23:34:04 -0600

    color update

--- collect/lib/main.dart
+++ collect/lib/main.dart
@@ -9,8 +9,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterShare',
       debugShowCheckedModeBanner: false,
+      title: 'Collection App',
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          accentColor: Colors.lightBlue,
+          canvasColor: Colors.lightGreen[50],
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(fontSize: 15, color: Colors.black),
+              body2: TextStyle(color: Colors.black),
+              title: TextStyle(
+                fontSize: 17,
+                fontFamily: 'Montserrat',
+              ),
+              subtitle: TextStyle(
+                fontSize: 15,
+                fontFamily: 'Montserrat',
+                color: Colors.grey,
+              ),
+              display1: TextStyle(fontSize: 15, fontFamily: 'Monteserrat'),
+              caption: TextStyle(fontSize: 13))),
       home: Home(),
     );
   }
--- collect/lib/pages/home.dart
+++ collect/lib/pages/home.dart
@@ -7,8 +7,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget authScreen() {
+    return Text("Authorized");
+  }
+
+  Scaffold unAuthScreen() {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Theme.of(context).accentColor, Theme.of(context).primaryColor])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Collect',
+              style: TextStyle(
+                  fontFamily: "funFont", fontSize: 90, color: Colors.white),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            GestureDetector(
+              onTap: (){},
+              child: Container(
+                  width: 180,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/google_signin_button.png'),
+                          fit: BoxFit.cover))),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth ? authScreen() : unAuthScreen();
   }
 }
