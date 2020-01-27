65be574a4664c9c5b716c485e3448e5fbb2bcfa7
Jack <jackyangzzh@gmail.com>
2020-01-27 14:31:23 -0600

    firebase query

--- collect/lib/pages/timeline.dart
+++ collect/lib/pages/timeline.dart
@@ -1,6 +1,9 @@
 import 'package:collect/widgets/header.dart';
 import 'package:collect/widgets/progress.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,10 +12,25 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() async {
+    QuerySnapshot snap = await usersRef
+        .orderBy('postCount' ,descending: true)
+        .getDocuments();
+    snap.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+    });
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isTitle: true),
-      body: linearProgress(context),
+      body: Text('Timeline'),
     );
   }
 }
