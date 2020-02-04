674e792aec13e62ba72663d02101d03cdd6713b9
Jack <jackyangzzh@gmail.com>
2020-02-03 18:46:21 -0600

    upload in progress

--- collect/lib/pages/upload.dart
+++ collect/lib/pages/upload.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
 
 import 'package:collect/models/user.dart';
+import 'package:collect/widgets/progress.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as image;
+import 'package:uuid/uuid.dart';
 
 class Upload extends StatefulWidget {
   final User currentUser;
@@ -15,6 +19,8 @@ class Upload extends StatefulWidget {
 
 class _UploadState extends State<Upload> {
   File file;
+  bool isLoading;
+  String postId = Uuid().v4();
 
   void handleTakePhoto() async {
     File file = await ImagePicker.pickImage(source: ImageSource.camera);
@@ -36,6 +42,20 @@ class _UploadState extends State<Upload> {
     });
   }
 
+  void handleSubmit() {
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  void compressImage() async {
+    final tempDir = await getTemporaryDirectory();
+    final path = tempDir.path;
+    image.Image imageFile = image.decodeImage(file.readAsBytesSync());
+    File('$path/img_$postId.jpg')
+      ..writeAsBytesSync(image.encodeJpg(imageFile, quality: 100));
+  }
+
   Scaffold buildUploadPage() {
     return Scaffold(
       appBar: AppBar(
@@ -53,18 +73,50 @@ class _UploadState extends State<Upload> {
               size: 32,
               color: Colors.white,
             ),
-            onPressed: () {},
+            onPressed: isLoading ? null : () => handleSubmit(),
           )
         ],
       ),
       body: SingleChildScrollView(
         child: Column(children: <Widget>[
+          isLoading ? linearProgress : Text(""),
           SizedBox(
             child: Image(
               image: FileImage(file),
               fit: BoxFit.cover,
             ),
-          )
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            alignment: Alignment.center,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Caption", border: InputBorder.none),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            // leading: Icon(Icons.location_on),
+            title: Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Location", border: InputBorder.none)),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.location_searching),
+              onPressed: () {},
+            ),
+          ),
+          Divider(),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            alignment: Alignment.center,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Description", border: InputBorder.none),
+            ),
+          ),
         ]),
       ),
     );
