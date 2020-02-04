e9628bce777172b217e584b182a805af5b1f020a
Jack <jackyangzzh@gmail.com>
2020-02-04 17:19:17 -0600

    word limit

--- collect/lib/pages/upload.dart
+++ collect/lib/pages/upload.dart
@@ -4,6 +4,7 @@ import 'package:collect/models/user.dart';
 import 'package:collect/widgets/progress.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image/image.dart' as image;
@@ -20,15 +21,12 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
-  final _form = GlobalKey<FormState>();
-
   TextEditingController locationController = TextEditingController();
   TextEditingController captionController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
 
   File file;
   bool isLoading = false;
-  bool autoValidate = false;
   String postId = Uuid().v4();
 
   void handleTakePhoto() async {
@@ -45,11 +43,7 @@ class _UploadState extends State<Upload> {
     });
   }
 
-  Future<void> clearImage() async {
-    final _isValid = _form.currentState.validate();
-    if (!_isValid) {
-      return;
-    }
+  void clearImage() {
     setState(() {
       file = null;
     });
@@ -72,6 +66,7 @@ class _UploadState extends State<Upload> {
     setState(() {
       isLoading = false;
       file = null;
+      postId = Uuid().v4();
     });
   }
 
@@ -135,70 +130,58 @@ class _UploadState extends State<Upload> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Form(
-          key: _form,
-          autovalidate: autoValidate,
-          child: Column(children: <Widget>[
-            isLoading ? linearProgress(context) : Text(""),
-            SizedBox(
-              child: Image(
-                image: FileImage(file),
-                fit: BoxFit.cover,
-              ),
+        child: Column(children: <Widget>[
+          isLoading ? linearProgress(context) : Text(""),
+          SizedBox(
+            child: Image(
+              image: FileImage(file),
+              fit: BoxFit.cover,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              alignment: Alignment.center,
-              child: TextFormField(
-                style: TextStyle(fontSize: 20),
-                controller: captionController,
-                decoration: InputDecoration(
-                    errorText: captionController.text.length > 20
-                        ? "Enter something shorter!"
-                        : null,
-                    hintText: "Caption",
-                    border: InputBorder.none),
-                validator: (_value) {
-                  if (_value.length < 2) {
-                    return 'Please enter a title';
-                  }
-                  if (_value.length > 10) {
-                    return 'Please keep the title short (less than 60 characters)';
-                  }
-                  return null;
-                },
-              ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            alignment: Alignment.center,
+            child: TextField(
+              maxLength: 60,
+              maxLines: 2,
+              // inputFormatters: [LengthLimitingTextInputFormatter(10)],
+              style: TextStyle(fontSize: 20),
+              controller: captionController,
+              decoration: InputDecoration(
+                  hintText: "Caption",
+                  border: InputBorder.none,
+                  counterStyle: TextStyle()),
             ),
-            Divider(),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              alignment: Alignment.center,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Container(
+              width: MediaQuery.of(context).size.width * 0.8,
               child: TextField(
-                maxLines: 5,
-                keyboardType: TextInputType.multiline,
-                controller: descriptionController,
-                decoration: InputDecoration(
-                    hintText: "Description", border: InputBorder.none),
-              ),
+                  controller: locationController,
+                  decoration: InputDecoration(
+                      hintText: "Location", border: InputBorder.none)),
             ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.location_on),
-              title: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                    controller: locationController,
-                    decoration: InputDecoration(
-                        hintText: "Location", border: InputBorder.none)),
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.location_searching),
-                tooltip: "Current Location",
-                onPressed: () {},
-              ),
+            trailing: IconButton(
+              icon: Icon(Icons.location_searching),
+              tooltip: "Current Location",
+              onPressed: () {},
             ),
-          ]),
-        ),
+          ),
+          Divider(),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            alignment: Alignment.center,
+            child: TextField(
+              maxLines: 10,
+              keyboardType: TextInputType.multiline,
+              controller: descriptionController,
+              decoration: InputDecoration(
+                  hintText: "Description", border: InputBorder.none),
+            ),
+          ),
+        ]),
       ),
     );
   }
